c397e67b30ef887653c0ab197d7e548413d7a8ba
杨健 <1439821144@qq.com>
2019-08-15 08:55:33 +0800

    修改pm2配置

---
+++ pm2.config.js
@@ -0,0 +1,16 @@
+(module.exports = {
+  "apps": [
+    {
+      "name": "development_platform_server",
+      "script": "bin/www",
+      "instances": 1,
+      "autorestart": true,
+      "watch": true,
+      "out_file": "./logs/out.log",
+      "error_file": "./logs/error.log",
+      "log_date_format": "YYYY-MM-DD HH:mm:ss",
+      "max_memory_restart": "500M",
+      "ignore_watch": "logs"
+    }
+  ]
+})
