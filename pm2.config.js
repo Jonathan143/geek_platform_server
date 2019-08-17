25a7155344efef13b0a2229301216c8694830719
Jonathan143 <1439821144@qq.com>
2019-08-17 16:29:43 +0800

    PM2 配置

--- pm2.config.js
+++ pm2.config.js
@@ -10,7 +10,7 @@ module.exports = {
       error_file: './logs/error.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       max_memory_restart: '500M',
-      ignore_watch: 'logs'
+      ignore_watch: ['node_modules', 'logs', 'public']
     }
   ]
 }
