53984b9e31170ab05e817181cadcb163b63637e7
jonathan143 <1439821144@qq.com>
2019-10-26 13:03:04 +0800

    test

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,15 +10,13 @@ router.post('/update-code', async ctx => {
     global.config.SECRET_KEY,
     'df602458267e876c'
   )
-  console.log(result, ctx.request.header['x-gitee-token'])
-  console.log(ctx.request)
 
   if (result === ctx.request.header['x-gitee-token']) {
     const git = await exec('git pull')
-    const noUpdate = 'Already up to date'
+    const noUpdate = 'Already up'
     if (!git.stdout.includes(noUpdate)) {
-      exec('yarn && yarn pm2')
       ctx.body = 'success'
+      exec('yarn && yarn pm2')
     } else {
       ctx.body = noUpdate
     }
