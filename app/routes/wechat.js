b64a67626b83b22890493959922f5fdfdc2f4ca9
Jonathan <1439821144@qq.com>
2022-10-31 11:36:39 +0800

    feat: 添加消息类型

--- app/routes/wechat.js
+++ app/routes/wechat.js
@@ -1,8 +1,13 @@
 const router = require('koa-router')()
 const sha1 = require('sha1')
-const {Wechat} = require('wechat-jssdk')
+const {Wechat, MongoStore} = require('wechat-jssdk')
 const xml2js = require('xml2js')
 const xmlParser = new xml2js.Parser({explicitArray: false})
+const builder = new xml2js.Builder({
+  cdata: true,
+  rootName: 'xml',
+  headless: true
+})
 
 const wx = new Wechat({
   //第一个为设置网页授权回调地址
@@ -12,7 +17,17 @@ const wx = new Wechat({
   appSecret: '',
   card: false, //开启卡券支持，默认关闭
   payment: false, //开启支付支持，默认关闭
-  merchantId: '' //商户ID
+  merchantId: '', //商户ID
+  store: new MongoStore({
+    //dbName: 'myWechat', //default wechat
+    dbAddress: `${global.config.MONGODB_HOST}/geek_platform`, //set the whole connection uri by yourself
+    dbOptions: {
+      authSource: 'admin',
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    } //set mongoose connection config
+  })
 })
 
 router.prefix('/wechat')
@@ -27,7 +42,22 @@ router.get('/', async ctx => {
 router.post('/', async ctx => {
   const {xml = {}} = await xmlParser.parseStringPromise(ctx.request.body)
   console.log(xml)
-  const {MsgType, Event, FromUserName, ToUserName} = xml
+  const {
+    MsgType,
+    Event,
+    FromUserName,
+    ToUserName,
+    MediaId,
+    Content,
+    CreateTime
+  } = xml
+
+  const baseReply = {
+    MsgType,
+    CreateTime,
+    ToUserName: FromUserName,
+    FromUserName: ToUserName
+  }
 
   switch (MsgType) {
     /**
@@ -43,13 +73,19 @@ router.post('/', async ctx => {
       )
       ctx.body = 'success'
       break
+    case 'image':
+      ctx.body = builder.buildObject({
+        ...baseReply,
+        Image: {MediaId}
+      })
+      break
 
     default:
-      ctx.body = {
-        ...xml,
-        ToUserName: FromUserName,
-        FromUserName: ToUserName
-      }
+      ctx.body = builder.buildObject({
+        ...baseReply,
+        MsgType: 'text',
+        Content: Content || '我不知道你在说什么'
+      })
       break
   }
 })
@@ -59,8 +95,8 @@ router.get('/get-signature', async ctx => {
 })
 
 router.get('/oauth-callback', async ctx => {
-  console.log(ctx.request)
-  ctx.body = 'success'
+  const code = ctx.request.query.code
+  ctx.body = await wx.oauth.getUserInfo(code)
 })
 
 module.exports = router
